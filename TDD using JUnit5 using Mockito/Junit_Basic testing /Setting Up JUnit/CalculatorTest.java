e8732f212e9cece78e64b1a25acd95a14cf56df3
susmitha4304 <susmitha.karaka07@gmail.com>
2026-06-26 20:18:18 +0530

    Add files via upload

---
+++ TDD using JUnit5 using Mockito/Junit_Basic testing /Setting Up JUnit/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+
+        Calculator calculator = new Calculator();
+
+        assertEquals(5, calculator.add(2,3));
+
+    }
+
+}
